9227bdfda9fc6c34e565f1a0338c1611c6189dfd
Tom Yandell <tom@tomyandell.co>
2016-11-14 10:25:35 +0000

    refactor and error handling

--- logspout-logentries-autowire.go
+++ logspout-logentries-autowire.go
@@ -1,173 +1,189 @@
-
 package logspout
 
 import (
-    "crypto/tls"
-    "encoding/json"
-    "github.com/fsouza/go-dockerclient"
-    "github.com/gliderlabs/logspout/router"
-    "io/ioutil"
-    "net/http"
-    "time"
-    "fmt"
-    "errors"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/fsouza/go-dockerclient"
+	"github.com/gliderlabs/logspout/router"
 )
 
-type LeaObj struct {
-  route                    *router.Route
+type logentriesAdaptor struct {
+	defaultToken string
+	host         string
+	instanceID   string
+	route        *router.Route
 }
-
-var DockerHost string
-var InstanceID string
-var DefaultToken string
 
 func init() {
-  router.AdapterFactories.Register(LogentriesAutowire, "logentriesautowire")
-
-  endpoint := "unix:///var/run/docker.sock"
-  client, _ := docker.NewClient(endpoint)
-  DockerInfo, _ := client.Info()
-  DockerHost = DockerInfo.Name
-
-  var netClient = &http.Client{
-    Timeout: time.Second * 5,
-  }
-
-  resp, err := netClient.Get("http://instance-data/latest/meta-data/instance-id")
-  if resp != nil {
-    defer resp.Body.Close()
-  }
-
-  if err != nil {
-    InstanceID = "Not AWS"
-  } else {
-    temp, _ := ioutil.ReadAll(resp.Body)
-    InstanceID = string(temp)
-  }
-
+	router.AdapterFactories.Register(LogentriesAutowire, "logentriesautowire")
 }
 
-func LogentriesAutowire (route *router.Route) (router.LogAdapter, error)  {
+func LogentriesAutowire(route *router.Route) (router.LogAdapter, error) {
 
-  DefaultToken = route.Address
+	client, err := docker.NewClient("unix:///var/run/docker.sock")
+	if err != nil {
+		return nil, err
+	}
 
-  return &LeaObj{
-    route:         route,
-  }, nil
+	dockerInfo, err := client.Info()
+	if err != nil {
+		return nil, err
+	}
 
+	var httpClient = &http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	resp, err := httpClient.Get("http://instance-data/latest/meta-data/instance-id")
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	var instanceID string
+	if err != nil {
+		instanceID = "Not AWS"
+	} else {
+		temp, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		instanceID = string(temp)
+	}
+
+	return &logentriesAdaptor{
+		defaultToken: route.Address,
+		host:         dockerInfo.Name,
+		instanceID:   instanceID,
+		route:        route,
+	}, nil
 }
 
 type LogentriesMessage struct {
-  Id         string                 `json:"id,omitempty"`
-  Image      string                 `json:"image,omitempty"`
-  Created    string                 `json:"created,omitempty"`
-  Status     string                 `json:"status,omitempty"`
-  Node       string                 `json:"node,omitempty"`
-  InstanceId string                 `json:"instanceid,omitempty"`
-  Labels     map[string]string      `json:"labels,omitempty"`
-  Line       map[string]interface{} `json:"line,omitempty"`
+	ID         string                 `json:"id,omitempty"`
+	Image      string                 `json:"image,omitempty"`
+	Created    string                 `json:"created,omitempty"`
+	Status     string                 `json:"status,omitempty"`
+	Node       string                 `json:"node,omitempty"`
+	InstanceID string                 `json:"instanceid,omitempty"`
+	Labels     map[string]string      `json:"labels,omitempty"`
+	Line       map[string]interface{} `json:"line,omitempty"`
 }
 
+func (adaptor *logentriesAdaptor) Stream(logstream chan *router.Message) {
 
-func (a *LeaObj) Stream(logstream chan *router.Message) {
+	logger := NewLogger()
 
-  logger := NewLogger()
+	for m := range logstream {
 
-  for m := range logstream {
+		token := m.Container.Config.Labels["logentries.token"]
+		if token == "" {
+			token = adaptor.defaultToken
+		}
 
-    token := m.Container.Config.Labels["logentries.token"]
-    if token == "" {
-      token = DefaultToken
-    }
+		var logline map[string]interface{}
+		if err := json.Unmarshal([]byte(m.Data), &logline); err != nil {
+			logline = make(map[string]interface{})
+			logline["message"] = m.Data
+		}
 
-    containerID := m.Container.Config.Hostname
-    imageID := m.Container.Config.Image
-    created := m.Container.Created
-    labels := m.Container.Config.Labels
-    status := m.Container.State.Status
-
-    var logline map[string]interface{}
-    err := json.Unmarshal([]byte(m.Data), &logline)
-
-    if err != nil{
-      logline = make(map[string]interface{})
-      logline["message"] = m.Data
-    }
-
-    payload := LogentriesMessage{Id: containerID, Image: imageID, Created: created.String(), Status: status , Node: DockerHost, InstanceId: InstanceID, Labels: labels, Line: logline}
-    jsonPayload, jsonerr := json.Marshal(payload)
-
-    if jsonerr != nil {
-      panic(jsonerr)
-    }
-
-    msg_to_send := []byte(token + " " + string(jsonPayload) + "\n")
-    logger.Write(&msg_to_send)
-
-  }
+		jsonPayload, err := json.Marshal(LogentriesMessage{
+			ID:         m.Container.Config.Hostname,
+			Image:      m.Container.Config.Image,
+			Created:    m.Container.Created.String(),
+			Status:     m.Container.State.Status,
+			Node:       adaptor.host,
+			InstanceID: adaptor.instanceID,
+			Labels:     m.Container.Config.Labels,
+			Line:       logline,
+		})
+		if err != nil {
+			log.Panic(err)
+		}
+		msg := []byte(token + " " + string(jsonPayload) + "\n")
+		if err := logger.Write(&msg); err != nil {
+			log.Println("could not write log message:", err)
+		}
+	}
 
 }
 
 type Logger struct {
-  conn           *tls.Conn
-  messageChannel chan *[]byte
+	conn           *tls.Conn
+	messageChannel chan *[]byte
 }
 
 func NewLogger() *Logger {
-  l := Logger{
-    messageChannel: make(chan *[]byte, 1000),
-  }
-  go l.handleMessages()
-  return &l
+	l := Logger{
+		messageChannel: make(chan *[]byte, 1000),
+	}
+	go l.handleMessages()
+	return &l
 }
 
 func (l *Logger) handleMessages() {
-  const defaultBackoff = 100 * time.Millisecond
-  const maxBackoff = 5 * time.Second
-  nextBackoff := defaultBackoff
+	const defaultBackoff = 100 * time.Millisecond
+	const maxBackoff = 5 * time.Second
+	nextBackoff := defaultBackoff
 
-  var conn *tls.Conn
-  var err error
-  for {
-    msg := <-l.messageChannel
-    for {
-      if conn == nil {
-        conn, err = tls.Dial("tcp", "data.logentries.com:443", &tls.Config{})
-        if err != nil {
-          fmt.Println("open failed, backing off", nextBackoff, err)
-          time.Sleep(nextBackoff)
-          nextBackoff *= 2
-          if nextBackoff > maxBackoff {
-            nextBackoff = maxBackoff
-          }
-          continue
-        } else {
-          nextBackoff = defaultBackoff
-        }
-      }
-      if _, err := conn.Write(*msg); err != nil {
-        conn = nil
-        fmt.Println("write failed, backing off", nextBackoff, err)
-        time.Sleep(nextBackoff)
-        nextBackoff *= 2
-        if nextBackoff > maxBackoff {
-          nextBackoff = maxBackoff
-        }
-        continue
-      } else {
-        nextBackoff = defaultBackoff
-      }
-      break
-    }
-  }
+	var conn *tls.Conn
+	var err error
+	for {
+		msg := <-l.messageChannel
+		nextBackoff = defaultBackoff
+		tries := 0
+	SendMessage:
+		for {
+			if conn == nil {
+				conn, err = tls.Dial("tcp", "data.logentries.com:443", &tls.Config{})
+				if err != nil {
+					log.Println("open failed, backing off", nextBackoff, err)
+					time.Sleep(nextBackoff)
+					nextBackoff *= 2
+					if nextBackoff > maxBackoff {
+						nextBackoff = maxBackoff
+					}
+					continue
+				}
+			}
+			totalWritten := 0
+			for totalWritten < len(*msg) {
+				written, err := conn.Write((*msg)[totalWritten:])
+				if err != nil {
+					tries += 1
+					conn = nil
+					log.Println("write failed, backing off", nextBackoff, err)
+					time.Sleep(nextBackoff)
+					nextBackoff *= 2
+					if nextBackoff > maxBackoff {
+						nextBackoff = maxBackoff
+					}
+					if tries >= 100 {
+						log.Println("unalbe to send message after 100 tries, discarding")
+						break SendMessage
+					}
+					continue SendMessage
+				}
+				nextBackoff = defaultBackoff
+				totalWritten += written
+			}
+			// success
+			break SendMessage
+		}
+	}
 }
 
 func (l *Logger) Write(data *[]byte) error {
-  select {
-  case l.messageChannel <- data:
-    return nil
-  default:
-    return errors.New("message buffer full, dropping message")
-  }
+	select {
+	case l.messageChannel <- data:
+		return nil
+	default:
+		return errors.New("message buffer full, dropping message")
+	}
 
 }
